afe02bcb89ded7b36af42bfd708036afc1971e30
Andrii Yurchuk <ay@mntw.re>
2024-02-13 20:32:05 +0100

    Initial commit

---
+++ roundtrip/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, Response, render_template, request
+
+from .route import generate_gpx
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        longitude = request.form["longitude"]
+        latitude = request.form["latitude"]
+        length = request.form["length"]
+
+        gpx_data = generate_gpx([float(longitude), float(latitude)], int(length) * 1000)
+        return Response(
+            gpx_data,
+            mimetype="application/gpx+xml",
+            headers={"Content-Disposition": f"attachment; filename={longitude}_{latitude}_{length}km.gpx"},
+        )
+
+    return render_template("index.html")
---
+++ roundtrip/route.py
@@ -0,0 +1,34 @@
+import random
+import sys
+from typing import Optional
+
+import requests
+from environs import Env
+
+env = Env()
+
+ORS_URL = "https://api.openrouteservice.org/v2/directions/foot-hiking/gpx"
+ORS_TOKEN = env.str("ORS_TOKEN")
+
+
+def generate_gpx(
+    start_end_location: list[float],
+    length: int,
+    num_points: Optional[int] = None,
+    seed: Optional[int] = None,
+) -> str:
+    if num_points is None:
+        num_points = random.randint(2, 32768)  # figure out what the max value is
+
+    if seed is None:
+        seed = random.randint(0, sys.maxsize)
+
+    data = {
+        "coordinates": [start_end_location],
+        "options": {"round_trip": {"length": length, "points": num_points, "seed": seed}},
+    }
+
+    resp = requests.post(ORS_URL, json=data, headers={"Authorization": ORS_TOKEN})
+    resp.raise_for_status()
+
+    return resp.text
